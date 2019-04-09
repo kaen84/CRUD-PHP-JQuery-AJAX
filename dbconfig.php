b0b0793c5fcb5bfe94baf4d19a1b2be6cab7e900
kaen84 <49137871+kaen84@users.noreply.github.com>
2019-04-09 21:40:15 +0300

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -5,7 +5,7 @@ $databaseHost = $_ENV["RDS_HOSTNAME"];
 $databaseName = $_ENV["RDS_DB_NAME"];
 $databaseUsername = $_ENV["RDS_USERNAME"];
 $databasePassword = $_ENV["RDS_PASSWORD"];
-	$con = mysqli_connect('localhost','root','','crud-ajax');
+	$con = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
 
 	if(!$con)
 	{
